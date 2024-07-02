20d7e4b438546624fdcba5deeac421d9b62f2870
Megha-0204 <megha.jain423@gmail.com>
2024-07-02 14:58:13 +0530

    changes in index and js and css files

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,11 +26,39 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
-
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                        : header.classList.remove('blur-header');
+}
+window.addEventListener('scroll', blurHeader)
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
 
+    const sendEmail = (e) => {
+        e.preventDefault()
 
+        emailjs.sendForm('service_zm2kpx9','template_rdgv87y','#contact-form','KAc9QlsJUig4ODJKs')
+        .then(() => {
+
+            //show sent message
+            contactMessage.textContent = 'Message sent successfully'
+
+            //remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            //clear input fields
+            contactForm.reset()
+        }, () => {
+            //show error message
+            contactMessage.textContent = 'Message not sent (service error)'
+        })
+    }
+
+    contactForm.addEventListener('submit',sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
